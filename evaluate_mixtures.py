b36614d15ec8c824c402cced4c887e4b3bf08a19
trenusch <trenusch@techfak.uni-bielefeld.de>
2022-08-14 21:06:34 +0200

    update mixing of metrics

--- evaluate_mixtures.py
+++ evaluate_mixtures.py
@@ -5,81 +5,153 @@ import tqdm
 import os
 import datasets
 
+def evaluate_mix(nli_scores, nli_scores_ad, metric_scores, metric_scores_ad, metric, dataset):
+    for i in range(11):
+        acc, kendall = {}, {}
+        weight = i * 0.1
+        metric_hash = "{}_nli_{}_{}".format(weight, 1 - weight, metric)
+        scores = [0.5 * (weight * nli + (1 - weight) * m) for nli, m in zip(nli_scores[2], metric_scores)]
+        scores_ad = [0.5 * (weight * nli + (1 - weight) * m) for nli, m in
+                     zip(nli_scores_ad[2], metric_scores_ad)]
+        acc[error], kendall[error] = calculate_accuracy_and_kendall(scores, scores_ad)
+
+        print_and_save(metric, metric_hash, dataset, len(refs), [error], acc, kendall,
+                       output_dir="data/mix_output.txt")
 
 def score_adv(scorer, error, dataset, refs, hyps, hyps_ad, sources):
+    from metrics.nli1_score import NLI1Scorer
+    nli_scorer = NLI1Scorer()
+
+    nli_scores = nli_scorer.evaluate_batch(refs, hyps)
+    nli_scores_ad = nli_scorer.evaluate_batch(refs, hyps_ad)
+
     if scorer == "NLI_BartScore":
-        pass
+        metric = scorer
+        from metrics.bart_score import BARTScorer
+        scorer = BARTScorer()
+
+        scores = scorer.evaluate_batch(refs, hyps)
+        scores_ad = scorer.evaluate_batch(refs, hyps)
+
+        evaluate_mix(nli_scores, nli_scores_ad, scores, scores_ad, metric, dataset)
 
     elif scorer == "NLI_BertScore":
-        pass
+        metric = scorer
+        from metrics.bert_score import BertScoreMetric
+        scorer = BertScoreMetric()
+
+        scores = scorer.evaluate_batch(refs, hyps, aggregate=False)
+        scores_ad = scorer.evaluate_batch(refs, hyps_ad, aggregate=False)
+
+        variants = ["bert_score_precision", "bert_score_recall", "bert_score_f1"]
+
+        for v in variants:
+            evaluate_mix(nli_scores, nli_scores_ad, scores, scores_ad, metric + v, dataset)
 
     elif scorer == "NLI_MoverScore2":
-        pass
+        metric = scorer
+        from metrics.moverscore_v2_score import get_idf_dict, word_mover_score
+
+        idf_dict_hyp = get_idf_dict(hyps)
+        idf_dict_hyp_ad = get_idf_dict(hyps_ad)
+        idf_dict_ref = get_idf_dict(refs)
+
+        scores = word_mover_score(refs, hyps, idf_dict_ref, idf_dict_hyp, stop_words=[], n_gram=1, remove_subwords=True)
+        scores_ad = word_mover_score(refs, hyps_ad, idf_dict_ref, idf_dict_hyp_ad, stop_words=[], n_gram=1,
+                                     remove_subwords=True)
+
+        evaluate_mix(nli_scores, nli_scores_ad, scores, scores_ad, metric, dataset)
 
     elif scorer == "NLI_CHRF":
-        from metrics.nli1_score import NLI1Scorer
+        metric = scorer
         from metrics.chrf_score import ChrfppMetric
 
-        metric = "NLI_CHRF"
-
-        nli_scorer = NLI1Scorer()
         chrf_scorer = ChrfppMetric()
 
-        nli_scores = nli_scorer.evaluate_batch(refs, hyps)
-        nli_scores_ad = nli_scorer.evaluate_batch(refs, hyps_ad)
         chrf_scores = chrf_scorer.evaluate_batch(refs, hyps, aggregate=False)
         chrf_scores_ad = chrf_scorer.evaluate_batch(refs, hyps_ad, aggregate=False)
         chrf_scores = [s['chrf'] for s in chrf_scores]
         chrf_scores_ad = [s['chrf'] for s in chrf_scores_ad]
 
-        for i in range(11):
-            acc, kendall = {}, {}
-            weight = i * 0.1
-            metric_hash = "{}_nli_{}_chrf".format(weight, 1 - weight)
-            scores = [0.5 * (weight * nli + (1 - weight) * chrf) for nli, chrf in zip(nli_scores[2], chrf_scores)]
-            scores_ad = [0.5 * (weight * nli + (1 - weight) * chrf) for nli, chrf in
-                         zip(nli_scores_ad[2], chrf_scores_ad)]
-            acc[error], kendall[error] = calculate_accuracy_and_kendall(scores, scores_ad)
-
-            print_and_save(metric, metric_hash, dataset, len(refs), [error], acc, kendall,
-                           output_dir="data/mix_output.txt")
+        evaluate_mix(nli_scores, nli_scores_ad, chrf_scores, chrf_scores_ad, metric, dataset)
 
     elif scorer == "NLI_Meteor":
-        pass
+        metric = scorer
+        from metrics.meteor_score import MeteorMetric
+        meteor_scorer = MeteorMetric()
+
+        meteor_scores = meteor_scorer.evaluate_batch(refs, hyps, aggregate=False)
+        meteor_scores_ad = meteor_scorer.evaluate_batch(refs, hyps_ad, aggregate=False)
+        meteor_scores = [b['meteor'] for b in meteor_scores]
+        meteor_scores_ad = [b['meteor'] for b in meteor_scores_ad]
+
+        evaluate_mix(nli_scores, nli_scores_ad, meteor_scores, meteor_scores_ad, metric, dataset)
 
     else:
         raise NotImplementedError
 
+def evaluate_corr_mix(nli_scores, metric_scores, metric, data):
+    for i in range(11):
+        weight = i * 0.1
+        metric_hash = "{}_nli_{}_{}".format(weight, 1 - weight, metric)
+        scores = [0.5 * (weight * nli + (1 - weight) * m) for nli, m in zip(nli_scores[2], metric_scores)]
+        evaluate(scores, data, metric, metric_hash, output_path="data/mix_human_correlation.csv")
 
-def score_corr():
+def score_corr(scorer, data):
+    from metrics.nli1_score import NLI1Scorer
+    nli_scorer = NLI1Scorer()
+    nli_scores = nli_scorer.evaluate_batch(refs, hyps)
+
     if scorer == "NLI_BartScore":
-        pass
+        metric = scorer
+        from metrics.bart_score import BARTScorer
+        scorer = BARTScorer()
+
+        scores = scorer.evaluate_batch(refs, hyps)
+
+        evaluate_corr_mix(nli_scores, scores, metric, data)
 
     elif scorer == "NLI_BertScore":
-        pass
+        metric = scorer
+        from metrics.bert_score import BertScoreMetric
+        scorer = BertScoreMetric()
+
+        scores = scorer.evaluate_batch(refs, hyps)
+
+        variants = ["bert_score_precision", "bert_score_recall", "bert_score_f1"]
+
+        for v in variants:
+            evaluate_corr_mix(nli_scores, scores, metric + v, data)
 
     elif scorer == "NLI_MoverScore2":
-        pass
+        metric = scorer
+        from metrics.moverscore_v2_score import get_idf_dict, word_mover_score
 
-    elif metric == "NLI_CHRF":
-        from metrics.nli1_score import NLI1Scorer
+        idf_dict_hyp = get_idf_dict(hyps)
+        idf_dict_ref = get_idf_dict(refs)
+
+        scores = word_mover_score(refs, hyps, idf_dict_ref, idf_dict_hyp, stop_words=[], n_gram=1, remove_subwords=True)
+
+        evaluate_corr_mix(nli_scores, scores, metric, data)
+
+    elif scorer == "NLI_CHRF":
+        metric = scorer
         from metrics.chrf_score import ChrfppMetric
-
-        nli_scorer = NLI1Scorer()
         chrf_scorer = ChrfppMetric()
 
-        nli_scores = nli_scorer.evaluate_batch(refs, hyps)
         chrf_scores = chrf_scorer.evaluate_batch(refs, hyps, aggregate=False)
         chrf_scores = [s['chrf'] for s in chrf_scores]
 
-        for i in range(11):
-            weight = i * 0.1
-            metric_hash = "{}_nli_{}_chrf".format(weight, 1 - weight)
-            scores = [0.5 * (weight * nli + (1 - weight) * chrf) for nli, chrf in zip(nli_scores[2], chrf_scores)]
-            evaluate(scores, data, metric, metric_hash, output_path="data/mix_human_correlation.csv")
+        evaluate_corr_mix(nli_scores, chrf_scores, metric, data)
 
     elif scorer == "NLI_Meteor":
-        pass
+        metric = scorer
+        from metrics.meteor_score import MeteorMetric
+        meteor_scorer = MeteorMetric()
+
+        meteor_scores = meteor_scorer.evaluate_batch(refs, hyps, aggregate=False)
+
+        evaluate_corr_mix(nli_scores, meteor_scores, metric, data)
 
     else:
         raise NotImplementedError
